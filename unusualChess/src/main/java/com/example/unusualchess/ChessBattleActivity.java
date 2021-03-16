52fa1d6e4270646ec4b6d1defd938eb54805342f
Oleksandr <keysindie@gmail.com>
2021-03-16 10:35:22 +0200

    Implement game table activity template

---
+++ unusualChess/src/main/java/com/example/unusualchess/ChessBattleActivity.java
@@ -0,0 +1,14 @@
+package com.example.unusualchess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChessBattleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chess_battle_activity);
+    }
+}
