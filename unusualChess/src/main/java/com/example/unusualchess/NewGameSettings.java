3e95749079332f0c67313540a7a3c0aa4c2c2332
Oleksandr <keysindie@gmail.com>
2021-03-16 11:53:39 +0200

    Add new game settings screen

---
+++ unusualChess/src/main/java/com/example/unusualchess/NewGameSettings.java
@@ -0,0 +1,14 @@
+package com.example.unusualchess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewGameSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game_settings);
+    }
+}
