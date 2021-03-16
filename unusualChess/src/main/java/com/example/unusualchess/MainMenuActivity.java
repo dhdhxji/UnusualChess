da90339d40f3e5859b5af520d12fdf2715761754
Oleksandr <keysindie@gmail.com>
2021-03-16 11:07:05 +0200

    Add transition between main menu and game activity

--- unusualChess/src/main/java/com/example/unusualchess/MainMenuActivity.java
+++ unusualChess/src/main/java/com/example/unusualchess/MainMenuActivity.java
@@ -5,26 +5,36 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //Disable title bar
         getSupportActionBar().hide();
-
         setContentView(R.layout.main_menu_activity);
 
+        Button play_button = findViewById(R.id.main_activity_startGame_btn);
+        play_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent chActivity = new Intent(this, ChessBattleActivity.class);
+
+        startActivity(chActivity);
     }
 }
