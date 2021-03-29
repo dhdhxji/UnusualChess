37ae8a20c1a745470727e68fe32a9db02cb51f5a
Oleksandr <keysindie@gmail.com>
2021-03-29 21:55:13 +0300

    Implement initial board setup in ChessModel

--- unusualChess/src/main/java/com/example/unusualchess/board/ChessModel.java
+++ unusualChess/src/main/java/com/example/unusualchess/board/ChessModel.java
@@ -1,6 +1,14 @@
 package com.example.unusualchess.board;
 
+import com.example.unusualchess.board.pieces.Bishop;
+import com.example.unusualchess.board.pieces.EmptyPiece;
+import com.example.unusualchess.board.pieces.King;
+import com.example.unusualchess.board.pieces.Knight;
+import com.example.unusualchess.board.pieces.Pawn;
+import com.example.unusualchess.board.pieces.Queen;
+import com.example.unusualchess.board.pieces.Rook;
 import com.example.unusualchess.common.MoveIntent;
+import com.example.unusualchess.common.Role;
 import com.example.unusualchess.util.ChessInvalidMoveException;
 import com.example.unusualchess.util.InvalidCellIndexException;
 import com.example.unusualchess.util.InvalidPlayerException;
@@ -37,6 +45,47 @@ public class ChessModel {
      * Reset game, setup initial pieces configuration
      */
     public void reset() {
-        //TODO: Implement stub
+        //setup pawns
+        for(int file = 0; file < BOARD_WIDTH; ++file) {
+            _pieces[1][file] = new Pawn(Role.WHITE);
+            _pieces[BOARD_WIDTH-2][file] = new Pawn(Role.BLACK);
+        }
+
+        //set rook`s
+        _pieces[0][0] = new Rook(Role.WHITE);
+        _pieces[0][BOARD_WIDTH-1] = new Rook(Role.WHITE);
+
+        _pieces[BOARD_WIDTH-1][0] = new Rook(Role.WHITE);
+        _pieces[BOARD_WIDTH-1][BOARD_WIDTH-1] = new Rook(Role.WHITE);
+
+        //set knight`s
+        _pieces[0][1] = new Knight(Role.WHITE);
+        _pieces[0][BOARD_WIDTH-2] = new Knight(Role.WHITE);
+
+        _pieces[BOARD_WIDTH-1][1] = new Bishop(Role.WHITE);
+        _pieces[BOARD_WIDTH-1][BOARD_WIDTH-2] = new Bishop(Role.WHITE);
+
+        //set bishop`s
+        _pieces[0][2] = new Bishop(Role.WHITE);
+        _pieces[0][BOARD_WIDTH-3] = new Bishop(Role.WHITE);
+
+        _pieces[BOARD_WIDTH-1][2] = new Bishop(Role.WHITE);
+        _pieces[BOARD_WIDTH-1][BOARD_WIDTH-3] = new Bishop(Role.WHITE);
+
+        //set queen`s
+        _pieces[0][3] = new Queen(Role.WHITE);
+        _pieces[BOARD_WIDTH-1][3] = new Queen(Role.BLACK);
+
+        //set kings
+        _pieces[0][3] = new King(Role.WHITE);
+        _pieces[BOARD_WIDTH-1][4] = new King(Role.BLACK);
     }
+
+
+
+
+
+    public static final int BOARD_WIDTH = 8;
+
+    private Piece[][] _pieces = new EmptyPiece[BOARD_WIDTH][BOARD_WIDTH];
 }
