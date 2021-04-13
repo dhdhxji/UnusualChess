254f552001c057f59b36733b189bb7ae5cb6d4c2
Oleksandr <keysindie@gmail.com>
2021-04-13 11:35:11 +0300

    feat: Implement Bishop available move generation

--- unusualChess/src/main/java/com/example/unusualchess/board/pieces/Bishop.java
+++ unusualChess/src/main/java/com/example/unusualchess/board/pieces/Bishop.java
@@ -6,6 +6,7 @@ import com.example.unusualchess.board.Piece;
 import com.example.unusualchess.common.Role;
 import com.example.unusualchess.util.ChessMoveEvent;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -30,7 +31,34 @@ public class Bishop extends Piece {
     public Set<CellIndex> getAvailableMoves(CellIndex pos,
                                             BoardHolder<Piece> board,
                                             List<ChessMoveEvent<Piece>> moveHistory) {
-        //TODO: Implement stub
-        return null;
+        Set<CellIndex> directions = new HashSet<>();
+        for(CellIndex base: CellIndex.genBasisCellVectors()) {
+            Set<CellIndex> derivatives = CellIndex.genBasisCellVectors();
+            derivatives.remove(base);
+            for(CellIndex derivative: derivatives) {
+                directions.add(base.add(derivative));
+                directions.add(base.add(derivative.inverse()));
+                directions.add(base.inverse().add(derivative));
+                directions.add(base.inverse().add(derivative.inverse()));
+            }
+        }
+
+        //For every direction generate all possible moves
+        Set<CellIndex> moves = new HashSet<>();
+
+        for(CellIndex d: directions) {
+            CellIndex dst = pos.add(d);
+            while(isMovePossible(dst, board) || isBeatPossible(dst, board)) {
+                moves.add(dst);
+
+                if(isBeatPossible(dst, board)) {
+                    break;
+                }
+
+                dst = dst.add(d);
+            }
+        }
+
+        return moves;
     }
 }
