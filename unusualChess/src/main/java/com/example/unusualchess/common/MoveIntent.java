29dd5a0c4bba667509b797960a18a131271e37eb
Oleksandr <keysindie@gmail.com>
2021-05-08 19:12:52 +0300

    feat: Add transformation selection to move intent

--- unusualChess/src/main/java/com/example/unusualchess/common/MoveIntent.java
+++ unusualChess/src/main/java/com/example/unusualchess/common/MoveIntent.java
@@ -3,14 +3,20 @@ package com.example.unusualchess.common;
 import androidx.annotation.NonNull;
 
 import com.example.unusualchess.board.CellIndex;
+import com.example.unusualchess.board.Piece;
 
 import java.util.Objects;
 
 public class MoveIntent {
     public MoveIntent(Role role, CellIndex src, CellIndex dst) {
+        this(role, src, dst, null);
+    }
+
+    public MoveIntent(Role role, CellIndex src, CellIndex dst, Piece transform) {
         _role = role;
         _srcPos = src;
         _dstPos = dst;
+        _transformTo = transform;
     }
 
     public Role getRole() {
@@ -25,13 +31,18 @@ public class MoveIntent {
         return _dstPos;
     }
 
-    @Override
+    public Piece getTransformTo() {
+        return _transformTo;
+    }
+
     @NonNull
+    @Override
     public String toString() {
         return "MoveIntent{" +
-                _role +
-                ", src:" + _srcPos +
-                ", dst:" + _dstPos +
+                "_role=" + _role +
+                ", _srcPos=" + _srcPos +
+                ", _dstPos=" + _dstPos +
+                ", _transformTo=" + _transformTo +
                 '}';
     }
 
@@ -41,18 +52,18 @@ public class MoveIntent {
         if (o == null || getClass() != o.getClass()) return false;
         MoveIntent that = (MoveIntent) o;
         return _role == that._role &&
-                _srcPos.equals(that._srcPos) &&
-                _dstPos.equals(that._dstPos);
+                Objects.equals(_srcPos, that._srcPos) &&
+                Objects.equals(_dstPos, that._dstPos) &&
+                Objects.equals(_transformTo, that._transformTo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(_role, _srcPos, _dstPos);
+        return Objects.hash(_role, _srcPos, _dstPos, _transformTo);
     }
 
     private final Role _role;
-
     private final CellIndex _srcPos;
     private final CellIndex _dstPos;
-
+    private final Piece _transformTo;
 }
